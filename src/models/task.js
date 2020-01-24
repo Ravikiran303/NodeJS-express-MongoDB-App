0a5ab5e6a0aef0b452227fd50ad222089aaab891
RAVIKIRAN PILLI <ravi.pilli@moduluspi.com>
2020-01-24 10:44:10 +0530

    Added task schema

---
+++ src/models/task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Task = new Schema({
+    description: { type: String, required: true, trim: true },
+    completed: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Tasks', Task);
