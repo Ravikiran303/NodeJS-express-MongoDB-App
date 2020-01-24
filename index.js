e0d67d0fe789342d23490b4851e725113514a7e5
RAVIKIRAN PILLI <ravi.pilli@moduluspi.com>
2020-01-24 15:14:26 +0530

    Modified code with async/await promises

--- index.js
+++ index.js
@@ -11,43 +11,86 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user);
-    }).catch((err) => {
+    } catch (err) {
         res.status(400).send(err)
-    })
+    }
+    // user.save().then(() => {
+    //     res.status(201).send(user);
+    // }).catch((err) => {
+    //     res.status(400).send(err)
+    // })
 });
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
         res.status(200).send(users);
-    }).catch(err => {
+    }
+    catch (err) {
         res.status(500).send(err);
-    })
+    }
+
+    // User.find({}).then((users) => {
+    //     res.status(200).send(users);
+    // }).catch(err => {
+    //     res.status(500).send(err);
+    // })
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send();
         }
         res.send(user);
-    }).catch((err) => {
+    }
+    catch (err) {
         res.status(500).send()
-    })
+    }
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send(user);
+    // }).catch((err) => {
+    //     res.status(500).send()
+    // })
+});
+
+app.put('/users/:id/:age', async (req, res) => {
+    console.log(req.params);
+    const _id = req.params.id;
+    const age = req.params.age;
+    const result = await User.findByIdAndUpdate(_id, { age });
+    if (!result) {
+        return res.status(304).send("Failed")
+    }
+    res.send(result)
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
 
     const task = new Task(req.body);
-    task.save().then(() => {
+    try {
+        await task.save();
         res.status(201).send(task);
-    }).catch((err) => {
+    }
+    catch (err) {
         res.status(400).send(err)
-    });
+    }
+
+    // task.save().then(() => {
+    //     res.status(201).send(task);
+    // }).catch((err) => {
+    //     res.status(400).send(err)
+    // });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
