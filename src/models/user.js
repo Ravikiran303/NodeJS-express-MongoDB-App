e25bb483cca26fea2b97cb84c465b03a54c86606
RAVIKIRAN PILLI <ravi.pilli@moduluspi.com>
2020-01-28 13:38:26 +0530

    Added tokenization to all api methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 var validator = require('validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+
 const Schema = mongoose.Schema;
 
 const User = new Schema({
     name: { type: String, required: true, trim: true },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -31,6 +36,36 @@ const User = new Schema({
         type: Number,
         required: true,
         default: 0
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+User.methods.generateAuthToken = async function () {
+    const user = this;
+    var token = jwt.sign({ _id: user._id }, 'MyToken');
+    user.tokens = user.tokens.concat({ token });
+    return token;
+}
+
+User.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.tokens;
+    return userObject;
+}
+
+User.pre('save', async function (next) {
+    const user = this;
+    console.log("Just before saving..")
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
     }
-})
+    next();
+});
 module.exports = mongoose.model('UsersCollection', User); 
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,11 +2,16 @@ const express = require('express')
 const router = new express.Router();
 const User = require('../models/user');
 
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var auth = require('../middleware/auth');
+
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
-        await user.save()
-        res.status(201).send(user);
+        await user.save();
+        var token = await user.generateAuthToken()
+        res.status(201).send({ user, token });
     } catch (err) {
         res.status(400).send(err)
     }
@@ -17,10 +22,24 @@ router.post('/users', async (req, res) => {
     // })
 });
 
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({});
         res.status(200).send(users);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+
+    // User.find({}).then((users) => {
+    //     res.status(200).send(users);
+    // }).catch(err => {
+    //     res.status(500).send(err);
+    // })
+});
+router.get('/users/me', auth, async (req, res) => {
+    try {
+        res.send(req.user);
     }
     catch (err) {
         res.status(500).send(err);
@@ -66,27 +85,92 @@ router.patch('/users/:id', async (req, res) => {
 
     try {
         const _id = req.params.id;
-        const result = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
-        if (!result) {
+        const user = await User.findById(req.params.id);
+
+        updates.forEach(update => { user[update] = req.body[update] });
+
+        await user.save();
+
+        // const result = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true });
+        if (!user) {
             return res.status(404).send("Failed")
         }
-        res.send(result)
+        res.send(user)
     } catch (err) {
         res.status(500).send(err);
     }
 
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        // const user = await User.findByIdAndDelete(req.user._id);
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
+        await req.user.remove();
+        res.send(req.user);
+    } catch (err) {
+        res.status(500).send()
+    }
+})
+router.delete('/users/:id', auth, async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
         if (!user) {
             return res.status(404).send()
         }
+        // await req.user.remove();
         res.send(user);
     } catch (err) {
         res.status(500).send()
     }
 })
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send()
+        }
+        //const user = User.findByCredentials(req.body.email, req.body.password);
+        // var token = jwt.sign({ _id: user._id }, 'MyToken');
+        // user.tokens = user.tokens.concat({ token });
+        var token = await user.generateAuthToken()
+        await user.save();
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(404).send("Password match failed")
+        }
+        res.send({ user, token });
+    }
+    catch (e) {
+        res.status(500).send();
+    }
+})
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+        res.send();
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save();
+        res.send();
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
+
 
 module.exports = router;
